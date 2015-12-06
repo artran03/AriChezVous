07ad30b9ba3e985bb714fca3c471d5c1c0be9390
adejacques <alexdejacques@gmail.com>
2015-12-06 19:37:08 +0100

    Ajout livre d'or

--- wp-content/themes/AriTheme/page-livre-dor.php
+++ wp-content/themes/AriTheme/page-livre-dor.php
@@ -1,22 +1,18 @@
-<?php 
+<?php
 
 get_header();
 
 if (have_posts()) :
-	
+
 	while (have_posts()) : the_post(); ?>
 
-	<h2><?php the_title(); ?></h2>
 	<p><?php the_content(); ?></p>
-	<p>Ceci est le livre d'or</p>
 
 	<?php endwhile;
 
 	else :
 		echo '<p>No content found</p>';
-	endif; 
+	endif;
 
 get_footer();
 ?>
-
-
