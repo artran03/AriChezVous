2e600c6ea9e156801a91661e992e6cc6b29eeebf
artran03 <arthur.tran91@gmail.com>
2015-12-20 16:37:59 +0100

    page souvenir responsive + Mobile header

--- wp-content/themes/AriTheme/header.php
+++ wp-content/themes/AriTheme/header.php
@@ -37,3 +37,25 @@
 					<!-- /Navigation Menu -->
 				</div>
 			</div>
+
+			<nav id="mobile-menu" class="navbar navbar-default navbar-fixed-top">
+		      <div class="container">
+		        <div class="navbar-header">
+		          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		            <span class="sr-only">Toggle navigation</span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		          </button>
+		          <a class="navbar-brand" href="<?php echo home_url(); ?>"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/logo.png" class="attachment-full" alt="logo"></a>
+		        </div>
+		        <div id="navbar" class="navbar-collapse collapse">
+		          <?php
+							$args = array(
+								'theme_location' => 'primary'
+							);
+						?>
+						<?php wp_nav_menu( $args ); ?>
+		        </div><!--/.nav-collapse -->
+		      </div>
+    		</nav>
--- wp-content/themes/AriTheme/page-souvenirs.php
+++ wp-content/themes/AriTheme/page-souvenirs.php
@@ -3,7 +3,7 @@
 get_header();
 
 ?>
-<div class="container-fluid souvenirs-container">
+
 
 <?php
 //$loop = new WP_Query( array( 'post_type' => 'souvenirs', 'posts_per_page' => 10 ) );
@@ -18,7 +18,7 @@ get_header();
 //endwhile;
 ?>
 
-
+<div id="three-per-div" class="container-fluid souvenirs-container">
 <?php 
 	$loop = new WP_Query( array( 'post_type' => 'souvenirs', 'posts_per_page' => 10 ) );
 	if (have_posts()) :
@@ -28,9 +28,11 @@ get_header();
 		<!-- <?php echo ($i+1).'-'; echo ($i+3); ?> -->
 		<div class="row souvenir-row">
 		<?php } ?>
-		<div class="souvenir col-lg-4">
+		<div class="souvenir col-lg-4 col-md-4 col-sm-6 col-xs-6">
 			<div id="<?php echo get_the_ID(); ?>" class="selfie-img"><?php the_post_thumbnail(); ?></div>
-			<div class="selfie-title"><?php the_title(); ?></div>
+			<div class="selfie-title-container">
+				<div class="selfie-title"><?php the_title(); ?></div>
+			</div>
 
 			<!-- Modal -->
 		      <div class="modal fade" id="modal<?php echo get_the_ID(); ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
@@ -67,6 +69,63 @@ get_header();
 	<?php endwhile; ?>
 		<?php
 		if($i%3!=0) { // put closing div here if loop is not exactly a multiple of 3 ?>
+		</div>
+		<?php } ?>
+
+<?php endif; ?>
+
+</div>
+
+<div id="two-per-div" class="container-fluid souvenirs-container">
+<?php 
+	$loop = new WP_Query( array( 'post_type' => 'souvenirs', 'posts_per_page' => 10 ) );
+	if (have_posts()) :
+	$i=0; // counter
+	while($loop->have_posts()) : $loop->the_post();
+		if($i%2==0) { // if counter is multiple of 3, put an opening div ?>
+		<!-- <?php echo ($i+1).'-'; echo ($i+3); ?> -->
+		<div class="row souvenir-row">
+		<?php } ?>
+		<div class="souvenir col-lg-4 col-md-6 col-sm-6 col-xs-12">
+			<div id="<?php echo get_the_ID(); ?>" class="selfie-img"><?php the_post_thumbnail(); ?></div>
+			<div class="selfie-title-container">
+				<div class="selfie-title"><?php the_title(); ?></div>
+			</div>
+			<!-- Modal -->
+		      <div class="modal fade" id="modal<?php echo get_the_ID(); ?>" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+		        <div class="modal-dialog">
+		          <div class="modal-content">
+		            <div class="modal-header">
+		              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+		            </div>
+		          <div class="modal-body">
+		            <div><?php the_content(); ?></div>
+		          </div>
+		        </div><!-- /.modal-content -->
+		      </div><!-- /.modal-dialog -->
+		    </div><!-- /.modal -->
+
+		    <script type="text/javascript">
+		    	jQuery(window).load(function(){
+				    jQuery('div#<?php echo get_the_ID(); ?>').on('click',function()
+	                {
+	                    jQuery('#modal<?php echo get_the_ID(); ?>').modal('show');
+	                    jQuery("#modal<?php echo get_the_ID(); ?> iframe").attr("src", jQuery("#modal<?php echo get_the_ID(); ?> iframe").attr("src"));
+	                });
+	            });
+	            jQuery("#modal<?php echo get_the_ID(); ?>").on('hidden.bs.modal', function (e) {
+				    jQuery("#modal<?php echo get_the_ID(); ?> iframe").attr("src", jQuery("#modal<?php echo get_the_ID(); ?> iframe").attr("src"));
+				});
+		    </script>
+		</div>
+		<?php $i++;
+		if($i%2==0) { // if counter is multiple of 2, put an closing div ?>
+		</div>
+		<?php } ?>
+
+	<?php endwhile; ?>
+		<?php
+		if($i%2!=0) { // put closing div here if loop is not exactly a multiple of 2 ?>
 		</div>
 		<?php } ?>
 
