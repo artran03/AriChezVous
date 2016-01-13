090ec1776c4fe43776841829acf324652361a79f
artran03 <arthurtran@macbook-pro-de-arthur.home>
2016-01-14 00:34:51 +0100

    Page à propos

---
+++ wp-content/themes/AriTheme/page-a-propos.php
@@ -0,0 +1,92 @@
+<?php get_header(); ?>
+
+<div id="acv-members" class="container-fluid">
+  <div class="row">
+    <div class="acv-member col-md-4 col-md-offset-4">
+      <div id="photo-ari" class="member-photo">
+        &nbsp;
+      </div>
+      <div class="name-func">
+        <span class="member-name">Ari</span>
+        <span class="hyphen">-</span>
+        <span class="member-function">Propriétaire</span>
+      </div>
+      <div class="additional-info"><span>(Initiateur du Projet & Conception)</span></div>
+      <div class="social-net-links"></div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="acv-member col-md-4 col-md-offset-2">
+      <div id="photo-perrine" class="member-photo">
+        &nbsp;
+      </div>
+      <div class="name-func">
+        <span class="member-name">Perrine</span>
+        <span class="hyphen">-</span>
+        <span class="member-function">Décoratrice</span>
+      </div>
+      <div class="additional-info"><span>(Design & Illustrations)</span></div>
+      <div class="social-net-links">
+        <span>
+          <a href="#"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/mail-01.png" width="25"/></a>
+          <a href="#"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/linkedin-logo-01.png" width="25"/></a>
+        </span>
+      </div>
+    </div>
+    <div class="acv-member col-md-4">
+      <div id="photo-arthur" class="member-photo">
+        &nbsp;
+      </div>
+      <div class="name-func">
+        <span class="member-name">Arthur</span>
+        <span class="hyphen">-</span>
+        <span class="member-function">Maçon</span>
+      </div>
+      <div class="additional-info"><span>(Conception & Développement)</span></div>
+      <div class="social-net-links">
+        <span>
+          <a href="mailto:arthur.tran91@gmail.com"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/mail-01.png" width="25"/></a>
+          <a href="https://www.linkedin.com/in/arthur-tran-77196955"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/linkedin-logo-01.png" width="25"/></a>
+        </span>
+      </div>
+    </div>
+  </div>
+  <div class="row" style=" margin-top: 2%;">
+    <div class="acv-member col-md-4 col-md-offset-2">
+      <div id="photo-amine" class="member-photo">
+        &nbsp;
+      </div>
+      <div class="name-func">
+        <span class="member-name">Amine</span>
+        <span class="hyphen">-</span>
+        <span class="member-function">Plombier</span>
+      </div>
+      <div class="additional-info"><span>(Conception & Développement)</span></div>
+      <div class="social-net-links">
+        <span>
+          <a href="#"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/mail-01.png" width="25"/></a>
+          <a href="#"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/linkedin-logo-01.png" width="25"/></a>
+        </span>
+      </div>
+    </div>
+    <div class="acv-member col-md-4">
+      <div id="photo-alex" class="member-photo">
+        &nbsp;
+      </div>
+      <div class="name-func">
+        <span class="member-name">Alexandre</span>
+        <span class="hyphen">-</span>
+        <span class="member-function">Electricien</span>
+      </div>
+      <div class="additional-info"><span>(Conception & Développement)</span></div>
+      <div class="social-net-links">
+        <span>
+          <a href="#"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/mail-01.png" width="25"/></a>
+          <a href="#"><img src="<?php echo home_url(); ?>/wp-content/themes/AriTheme/img/linkedin-logo-01.png" width="25"/></a>
+        </span>
+      </div>
+    </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
