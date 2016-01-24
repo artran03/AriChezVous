7bd1c37eaec37e969c1fc9dc750f1bef356e8fb7
adejacques <alexdejacques@gmail.com>
2016-01-24 12:12:54 +0100

    Changement resolution boutons res sociaux

--- wp-content/themes/AriTheme/footer.php
+++ wp-content/themes/AriTheme/footer.php
@@ -1,17 +1,17 @@
 	<footer class="site-footer">
 			<div id="icones-social">
 				<a href="http://www.facebook.com/ariofficiel" title="Suivez-moi sur facebook !" target="_blank">
-					<img src="<?php echo home_url();?>/wp-content/themes/AriTheme/img/facebook.png"
+					<img src="<?php echo home_url();?>/wp-content/themes/AriTheme/img/facebook.png" height="40" width="40"
 					onmouseover="this.src='<?php echo home_url();?>/wp-content/themes/AriTheme/img/facebook_hover.png';"
 					onmouseout="this.src='<?php echo home_url();?>/wp-content/themes/AriTheme/img/facebook.png';"/>
 				</a>
 				<a href="http://www.youtube.com/user/ariofficiel" title="Suivez-moi sur youtube !" target="_blank">
-					<img src="<?php echo home_url();?>/wp-content/themes/AriTheme/img/youtube.png"
+					<img src="<?php echo home_url();?>/wp-content/themes/AriTheme/img/youtube.png" height="40" width="40"
 					onmouseover="this.src='<?php echo home_url();?>/wp-content/themes/AriTheme/img/youtube_hover.png';"
 					onmouseout="this.src='<?php echo home_url();?>/wp-content/themes/AriTheme/img/youtube.png';"/>
 				</a>
 					<a href="http://www.twitter.com/officiel_ari" title="Suivez-moi sur twitter !" target="_blank">
-						<img src="<?php echo home_url();?>/wp-content/themes/AriTheme/img/twitter.png"
+						<img src="<?php echo home_url();?>/wp-content/themes/AriTheme/img/twitter.png" height="40" width="40"
 						onmouseover="this.src='<?php echo home_url();?>/wp-content/themes/AriTheme/img/twitter_hover.png';"
 						onmouseout="this.src='<?php echo home_url();?>/wp-content/themes/AriTheme/img/twitter.png';"/>
 					</a>
