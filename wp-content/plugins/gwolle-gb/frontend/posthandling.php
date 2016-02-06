7271b064acaf1302da1986601d6514ac7ac2f3c3
adejacques <alexdejacques@gmail.com>
2016-02-06 12:24:31 +0100

    Livre or moderate post

--- wp-content/plugins/gwolle-gb/frontend/posthandling.php
+++ wp-content/plugins/gwolle-gb/frontend/posthandling.php
@@ -286,6 +286,8 @@ function gwolle_gb_frontend_posthandling() {
 			}
 		}
 
+		$entry->set_ischecked( false );
+
 
 		/* Check for logged in user, and set the userid as author_id, just in case someone is also admin, or gets promoted some day */
 		$entry->set_author_id( $user_id );
